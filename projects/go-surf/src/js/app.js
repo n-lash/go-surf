843cc29a19f432a8d9c1cbecde31d04c0829f5b5
n-lash <natalas111222@gmail.com>
2020-07-21 13:00:18 +0300

    shop-section added

--- projects/go-surf/src/js/app.js
+++ projects/go-surf/src/js/app.js
@@ -9,10 +9,6 @@ $(document).ready(function(){
         prevArrow: '<img class="slider-arrow  slider-arrow--left" src="./img/icons/arrow-left.svg" alt="Arrow left">',
         nextArrow: '<img class="slider-arrow  slider-arrow--right" src="./img/icons/arrow-right.svg" alt="Arrow right">',
         asNavFor: '.slider-dots',
-
-        //prevArrow: '<button type="button" class="slick-prev"><img src="./img/icons/arrow-left.svg" alt="Arrow left"></button>',
-        //nextArrow: '<button type="button" class="slick-next"><img src="./img/icons/arrow-right.svg" alt="Arrow right"></button>',
-
     });
 
     $('.slider-dots').slick({
@@ -40,7 +36,7 @@ $(document).ready(function(){
         asNavFor: '.surf-map',
     });
 
-    $('.travel-slider').slick({
+    $('.travel-slider, .sleep-slider, .shop-slider').slick({
         infinite: true,
         arrows: true,
         fade: true,
@@ -50,25 +46,32 @@ $(document).ready(function(){
         nextArrow: '<img class="slider-arrow  slider-arrow--right" src="./img/icons/arrow-right.svg" alt="Arrow right">',
     });
 
-    $('.sleep-slider').slick({
-        infinite: true,
-        arrows: true,
-        fade: true,
-        speed: 500,
-        cssEase: 'linear',
-        prevArrow: '<img class="slider-arrow  slider-arrow--left" src="./img/icons/arrow-left.svg" alt="Arrow left">',
-        nextArrow: '<img class="slider-arrow  slider-arrow--right" src="./img/icons/arrow-right.svg" alt="Arrow right">',
-    });
+    // $('.sleep-slider').slick({
+    //     infinite: true,
+    //     arrows: true,
+    //     fade: true,
+    //     speed: 500,
+    //     cssEase: 'linear',
+    //     prevArrow: '<img class="slider-arrow  slider-arrow--left" src="./img/icons/arrow-left.svg" alt="Arrow left">',
+    //     nextArrow: '<img class="slider-arrow  slider-arrow--right" src="./img/icons/arrow-right.svg" alt="Arrow right">',
+    // });
 
-    $('.shop-slider').slick({
-        infinite: true,
-        arrows: true,
-        fade: true,
-        speed: 500,
-        cssEase: 'linear',
-        prevArrow: '<img class="slider-arrow  slider-arrow--left" src="./img/icons/arrow-left.svg" alt="Arrow left">',
-        nextArrow: '<img class="slider-arrow  slider-arrow--right" src="./img/icons/arrow-right.svg" alt="Arrow right">',
+    // $('.shop-slider').slick({
+    //     infinite: true,
+    //     arrows: true,
+    //     fade: true,
+    //     speed: 500,
+    //     cssEase: 'linear',
+    //     prevArrow: '<img class="slider-arrow  slider-arrow--left" src="./img/icons/arrow-left.svg" alt="Arrow left">',
+    //     nextArrow: '<img class="slider-arrow  slider-arrow--right" src="./img/icons/arrow-right.svg" alt="Arrow right">',
+    // });
+
+    $('.shop-slider__img-circle').on('click', function() {
+        $(this).toggleClass('active')
     });
 
 });
 
+
+    //prevArrow: '<button type="button" class="slick-prev"><img src="./img/icons/arrow-left.svg" alt="Arrow left"></button>',
+    //nextArrow: '<button type="button" class="slick-next"><img src="./img/icons/arrow-right.svg" alt="Arrow right"></button>',
