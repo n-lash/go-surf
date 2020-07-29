08b6b690cb015a1697a3bf1d4579c12de1da0f61
n-lash <natalas111222@gmail.com>
2020-07-29 22:48:56 +0300

    surf-accordion added

--- projects/go-surf/src/js/app.js
+++ projects/go-surf/src/js/app.js
@@ -69,8 +69,28 @@ $(document).ready(function(){
     $('.shop-slider__img-circle').on('click', function() {
         $(this).toggleClass('active')
     });
-
 });
+
+
+const accHead = document.getElementsByClassName('surf-accordion__head');
+
+if (accHead.length) {
+    for (let i = 0; i < accHead.length; i++) {
+        accHead[i].addEventListener('click', function() {
+            this.classList.toggle('current');
+
+            const accBody = this.nextElementSibling;
+            if (accBody.style.display === 'block') {
+                accBody.style.display = 'none';
+            } else {
+                accBody.style.display = 'block';
+            }
+        });
+    }
+}
+
+
+
 
 
     //prevArrow: '<button type="button" class="slick-prev"><img src="./img/icons/arrow-left.svg" alt="Arrow left"></button>',
